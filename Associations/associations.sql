040c1962b1e78f48f3e8ee6edb71b466d9b0db28
M-H-Boroumandnia <118667895+M-H-Boroumandnia@users.noreply.github.com>
2025-05-02 02:39:57 +0330

    add id for candidates table create table for voting process called votes

--- Associations/associations.sql
+++ Associations/associations.sql
@@ -1,5 +1,12 @@
+DROP TABLE IF EXISTS memberships CASCADE;
+DROP TABLE IF EXISTS votes CASCADE;
+DROP TABLE IF EXISTS candidates CASCADE;
+DROP TABLE IF EXISTS association_sessions CASCADE;
+DROP TABLE IF EXISTS associations CASCADE;
+DROP TABLE IF EXISTS students CASCADE;
+DROP TYPE IF EXISTS candidate_status CASCADE;
+
 CREATE TYPE candidate_status AS ENUM ('qualified', 'disqualified', 'withdrawn');
-DROP TABLE students, associations;
 
 CREATE TABLE students (
     id CHAR(9) PRIMARY KEY,
@@ -13,18 +20,20 @@ CREATE TABLE associations (
     name VARCHAR(50) NOT NULL UNIQUE,
     foundation_date DATE NOT NULL,
     member_count INT NOT NULL,
-    CHECK (member_count >= 0),
-    bylaws TEXT NOT NULL
+    bylaws TEXT NOT NULL,
+
+    CHECK (member_count >= 0)
 );
 
 CREATE TABLE association_sessions (
     id SERIAL PRIMARY KEY,
     association_id INT NOT NULL,
     session_number INT NOT NULL,
-    CHECK (session_number > 0),
     secretary_id CHAR(9),
     deputy_secretary_id CHAR(9),
     election_date DATE NOT NULL,
+
+    CHECK (session_number > 0),
     UNIQUE (association_id, session_number),
     FOREIGN KEY (association_id) REFERENCES associations(id) ON DELETE CASCADE,
     FOREIGN KEY (secretary_id) REFERENCES students(id) ON DELETE SET NULL,
@@ -32,12 +41,13 @@ CREATE TABLE association_sessions (
 );
 
 CREATE TABLE candidates (
+    id SERIAL PRIMARY KEY,
     student_id CHAR(9) NOT NULL,
     association_session_id INT NOT NULL,
     vote_count INT NOT NULL DEFAULT 0,
-    CHECK (vote_count >= 0), 
     status candidate_status NOT NULL,
-    PRIMARY KEY (student_id, association_session_id),
+
+    CHECK (vote_count >= 0),
     FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE,
     FOREIGN KEY (association_session_id) REFERENCES association_sessions(id) ON DELETE CASCADE
 );
@@ -48,6 +58,17 @@ CREATE TABLE memberships (
     alternative BOOLEAN NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
+
     PRIMARY KEY (student_id, association_session_id),
-    FOREIGN KEY (student_id, association_session_id) REFERENCES candidates(student_id, association_session_id) ON DELETE CASCADE
+    FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE,
+    FOREIGN KEY (association_session_id) REFERENCES association_sessions(id) ON DELETE CASCADE
+);
+
+CREATE TABLE votes (
+    candidate_id INT NOT NULL,
+    student_id CHAR(9) NOT NULL,
+
+    PRIMARY KEY (candidate_id, student_id),
+    FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE,
+    FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE
 );
